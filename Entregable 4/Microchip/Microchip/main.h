6ca8622978c8482b61f917dbe13e4de7c65a8d9c
agumurray <agumurray@gmail.com>
2025-08-03 13:30:21 -0300

    fix build errors

--- Entregable 4/Microchip/Microchip/main.h
+++ Entregable 4/Microchip/Microchip/main.h
@@ -1,29 +1,12 @@
-/*
- * main.h
- *
- * Created: 6/6/2025 16:04:59
- *  Author: mater
- */
-
 #ifndef MAIN_H
 #define MAIN_H
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <string.h>
-#include "UART_utils.h"
-#include "Timer1_utils.h"
-#include "Timer0_utils.h"
-#include "ADC_utils.h"
+#include <stdbool.h>
 
 #define BR9600 (0x67)
-
-typedef enum
-{
-  false,
-  true
-} bool;
-
 #define BUFFER_SIZE 256
 
-#endif /* MAIN_H_ */
+#endif /* MAIN_H */
